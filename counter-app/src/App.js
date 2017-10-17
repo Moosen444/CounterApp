0f0fc9324ae178f72003661f9a60e30e36006692
Andrew Rasmussen <moosen444@gmail.com>
2017-10-17 16:58:17 -0400

    Removed generic app intro and added my own

--- counter-app/src/App.js
+++ counter-app/src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Mewtwo Move Counter</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <code>An easy way to track usage of moves!</code>
         </p>
       </div>
     );
