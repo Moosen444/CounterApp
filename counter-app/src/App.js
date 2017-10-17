c407d71ac30658bba2dd4b1ea6e6fd564b469242
Andrew Rasmussen <moosen444@gmail.com>
2017-10-17 17:18:31 -0400

    Added first button for counter

--- counter-app/src/App.js
+++ counter-app/src/App.js
@@ -13,9 +13,12 @@ class App extends Component {
         <p className="App-intro">
           <code>An easy way to track usage of moves!</code>
         </p>
+        <button>Forward Air</button>
       </div>
     );
   }
 }
+
+
 
 export default App;
