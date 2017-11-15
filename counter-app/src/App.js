5ce5a7e2df0da1d8688e32f3db61f03cecd901af
Andrew Rasmussen <moosen444@gmail.com>
2017-11-14 22:47:28 -0500

    I changed a lot to correct a lot still not working

--- counter-app/src/App.js
+++ counter-app/src/App.js
@@ -1,40 +1,27 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+// this.click.bind(this)
 
 class App extends Component {
+  click(){
+    alert("whaddup");
+
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src="https://i.warosu.org/data/tg/img/0291/28/1388281435235.jpg" className="App-logo" alt="logo" />
           <h1 className="App-title">Mewtwo Move Counter</h1>
         </header>
-        <p className="App-intro">
-          <code>An easy way to track usage of moves!</code>
-        </p>
-        var MewtwoFair = React.createClass({
-    getInitialState: function() {
-        return {
-            numClicks: 0
-        }
-    }
-    click: function() {
-        this.setState(numClicks: this.state.numClicks + 1);
-    },
-    render: function() {
-       return  (
         <div>
-            <button onClick={this.click.bind(this)}>Mewtwo Fair</button>
-            {this.state.numClicks}
+            <button onClick={this.click}>Mewtwo Fair</button>
         </div>
-       );
-   }
-});
-        </div>
-      );
-    }
+      </div>
+    );
   }
+}
 
 
-    export default App;
+export default App;
