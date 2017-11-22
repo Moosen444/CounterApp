d82e97335766505e7c8a69bd541a70da850e9e3d
Andrew Rasmussen <moosen444@gmail.com>
2017-11-21 21:32:28 -0500

    finally made the buttons increment...better late than never

--- counter-app/src/App.js
+++ counter-app/src/App.js
@@ -3,11 +3,18 @@ import logo from './logo.svg';
 import './App.css';
 // this.click.bind(this)
 
-class App extends Component {
-  click(){
-    alert("whaddup");
-
+class Inc extends React.Component {
+ constructor(props){
+   super(props);
+   this.state = {counter : 0}
+   this.increment = this.increment.bind(this);
   }
+  increment(){
+    this.setState({
+      counter : this.state.counter + 1
+    });
+  }
+
   render() {
     return (
       <div className="App">
@@ -15,13 +22,19 @@ class App extends Component {
           <img src="https://i.warosu.org/data/tg/img/0291/28/1388281435235.jpg" className="App-logo" alt="logo" />
           <h1 className="App-title">Mewtwo Move Counter</h1>
         </header>
-        <div>
-            <button onClick={this.click}>Mewtwo Fair</button>
-        </div>
+        <span>
+            <button onClick={this.increment}>Mewtwo Fair {this.state.counter}</button>
+        </span>
+        <span>
+            <button onClick={this.increment}>Mewtwo Nair {this.state.counter}</button>
+        </span>
+        <span>
+            <button onClick={this.increment}>Mewtwo Shadow Ball {this.state.counter}</button>
+        </span>
       </div>
     );
   }
-}
+};
 
 
-export default App;
+export default Inc;
